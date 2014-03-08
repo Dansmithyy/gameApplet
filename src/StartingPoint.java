662a8083b5b93b3f67a7b06b6ab49b15dd1ad19f
Dansmithyy <dan@todayintech.co.uk>
2014-03-08 22:19:24 +0000

    adding files

---
+++ src/StartingPoint.java
@@ -0,0 +1,37 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+
+public class StartingPoint extends Applet{
+	
+	@Override
+	public void init() {
+
+
+	}
+	
+	@Override
+	public void start() {
+
+
+	}
+	
+	@Override
+	public void stop() {
+
+
+	}
+	
+	@Override
+	public void destroy() {
+
+
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+
+
+	}
+	
+}
